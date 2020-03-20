7ea709d6166295e47f8e5e4a795d9fee0dd4a99c
creanme <creanme@live.com>
2020-03-20 09:33:50 +0800

    style🌈:统一了缩进与引号

--- src/config/commit-input.ts
+++ src/config/commit-input.ts
@@ -1,16 +1,14 @@
-import { InputBoxOptions } from "vscode";
+import { InputBoxOptions } from 'vscode';
 
 /**
  * @description git commit input 输入提交信息的配置对象
  */
-export interface CommitInputType extends InputBoxOptions {
-}
+export interface CommitInputType extends InputBoxOptions {}
 const CommitInputType: CommitInputType = {
-  placeHolder: `请输入提交信息`,
-  ignoreFocusOut: true,
-  prompt: "",
-  value: ""
+    placeHolder: '请输入提交信息',
+    ignoreFocusOut: true,
+    prompt: '',
+    value: ''
 };
-
 
 export default CommitInputType;
