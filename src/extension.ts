aa505781f17150f8beb3f77a9595ce05e5e604c0
Redjue <pingfj77@gmail.com>
2022-12-12 10:37:41 +0800

    fix(#81): fix icon does not clicked

--- src/extension.ts
+++ src/extension.ts
@@ -37,9 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     //获取是否在git扩展内 Gets whether it is in the git extension
     function getGitExtension() {
-        const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
-        const gitExtension = vscodeGit && vscodeGit.exports;
-        return gitExtension;
+        return vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
     }
     //Commit message config
     const message_config: GitMessage = {
@@ -231,7 +229,8 @@ export function activate(context: vscode.ExtensionContext) {
             if (uri) {
                 //如果有多个repo 寻找当前的 进行填充 If there are multiple repos looking for the current to populate
                 repo = gitExtension.getAPI(1).repositories.find(repo => {
-                    return repo.rootUri.path === uri.rootUri.path;
+                    const uriRoot = uri._rootUri ? uri._rootUri : uri.rootUri;
+                    return repo.rootUri.path === uriRoot?.path;
                 });
             }
             startMessageInput();
